e12214f60f2b39d89d84741a6969343d313efbd7
Zhi Heng <tanzhiheng26@gmail.com>
2023-05-17 11:37:27 +0800

    Add HomeScreen and LoginScreen

---
+++ firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB0zFd7wzjwZZRFOWb5hw6lv3pDMdEkCnI",
+  authDomain: "fir-auth-5abf1.firebaseapp.com",
+  projectId: "fir-auth-5abf1",
+  storageBucket: "fir-auth-5abf1.appspot.com",
+  messagingSenderId: "1020326988746",
+  appId: "1:1020326988746:web:7acd0d861bedc69dd21d33"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export { auth };
---
+++ screens/LoginScreen.js
@@ -0,0 +1,133 @@
+import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import React, {useEffect, useState} from 'react'
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from '../firebase';
+import { useNavigation } from '@react-navigation/native';
+import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
+
+const LoginScreen = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const navigation = useNavigation();
+
+    useEffect(() => {
+      const unsubsribe = onAuthStateChanged(auth, user => {
+        if (user) {
+          navigation.replace("Home")
+        }
+      })
+      return unsubsribe;
+    }, [])
+
+    const handleSignUp = () => {
+        createUserWithEmailAndPassword(auth, email, password)
+        .then(userCredential => {
+            const user = userCredential.user;
+            console.log("Registered with: ", user.email);
+        })
+        .catch(error => alert(error.message))
+    }
+
+    const handleLogin = () => {
+      signInWithEmailAndPassword(auth, email, password)
+      .then(userCredential => {
+          const user = userCredential.user;
+          console.log("Logged in with: ", user.email);
+      })
+      .catch(error => alert(error.message))
+  }
+
+    return (
+      <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }}> 
+        <KeyboardAvoidingView
+          style={styles.container}
+          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        >
+          <View style={styles.inputContainer}>
+            <TextInput 
+              placeholder='Email'
+              value={email}
+              onChangeText={text => setEmail(text)}
+              style={styles.input}
+            />
+            <TextInput 
+              placeholder='Password'
+              value={password}
+              onChangeText={text => setPassword(text)}
+              style={styles.input}
+              secureTextEntry={true}
+            />             
+          </View>
+
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity 
+              onPress={handleLogin}
+              style={styles.button}
+            >
+              <Text style={styles.buttonText}>Login</Text>
+            </TouchableOpacity>
+            <TouchableOpacity 
+              onPress={handleSignUp}
+              style={[styles.button, styles.buttonOutline]}
+            >
+              <Text style={styles.buttonOutLineText}>Register</Text>
+            </TouchableOpacity>
+          </View>
+        </KeyboardAvoidingView>
+      </SafeAreaView>
+      </SafeAreaProvider>
+     
+  )
+}
+
+export default LoginScreen
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    inputContainer: {
+        width: '80%',
+    },
+    input: {
+        backgroundColor: 'white',
+        paddingHorizontal: 15,
+        paddingVertical: 10,
+        borderRadius: 10,
+        marginTop: 5,
+    },
+    buttonContainer: {
+        width: '60%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 40
+    },
+    button: {
+        backgroundColor: '#0782F9',
+        width: '100%',
+        padding: 15,
+        borderRadius: 10,
+        alignItems: 'center'
+    },
+    buttonOutline: {
+        backgroundColor: 'white',
+        marginTop: 5,
+        borderColor: '#0782F9',
+        borderWidth: 2
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: '700',
+        fontSize: 16
+    }, 
+    buttonOutLineText: {
+        color: '#0782F9',
+        fontWeight: '700',
+        fontSize: 16
+    }
+        
+})
