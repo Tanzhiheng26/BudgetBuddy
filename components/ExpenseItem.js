db88e02d502f13694d5d9f96aeed750476b0c2e8
youthfrost <cherhan10@gmail.com>
2023-05-25 18:18:33 +0800

    edit database

--- components/ExpenseItem.js
+++ components/ExpenseItem.js
@@ -3,21 +3,23 @@ import React, { useContext } from 'react'
 import { AppContext } from '../context/AppContext';
 import { deleteExpense } from '../firestore';
 import { Icon } from '@rneui/themed';
+import { auth } from '../firebase'
 
-const ExpenseItem = ({id, name, cost}) => {
+
+const ExpenseItem = ({ id, name, cost }) => {
     const { dispatch } = useContext(AppContext);
     const handleDeleteExpense = () => {
         dispatch({
             type: 'DELETE_EXPENSE',
             payload: id,
         })
-        deleteExpense(id);
+        deleteExpense(auth.currentUser?.uid, id);
     }
     return (
         <View style={styles.row}>
             <Text style={styles.text}>{name}: </Text>
             <Text style={styles.text}>${cost}</Text>
-            <Icon name='delete' onPress={ handleDeleteExpense }/>
+            <Icon name='delete' onPress={handleDeleteExpense} />
         </View>
     );
 }
@@ -30,6 +32,6 @@ const styles = StyleSheet.create({
         justifyContent: 'space-around',
     },
     text: {
-        fontSize: 18, 
+        fontSize: 18,
     },
 })
--- firestore.js
+++ firestore.js
@@ -1,15 +1,20 @@
-import { addDoc, collection, getDocs, query, where, doc, deleteDoc, updateDoc } from "firebase/firestore";
+import { addDoc, collection, getDocs, getDoc, query, where, doc, deleteDoc, updateDoc, setDoc } from "firebase/firestore";
 import { db } from "./firebase";
 
 const EXPENSES_COLLECTION = 'expenses'
 const BUDGET_COLLECTION = 'budgets'
+const USERS_COLLECTION = 'users'
 
-export function addExpense(uid, id, name, cost) {
+export async function addExpense(uid, id, name, cost) {
+    const userRef = collection(db, USERS_COLLECTION, uid, EXPENSES_COLLECTION)
+    await setDoc(doc(userRef, id), { uid, id, name, cost });
+    /*
     addDoc(collection(db, EXPENSES_COLLECTION), { uid, id, name, cost });
-}  
+    */
+}
 
 export async function getExpense(uid) {
-    const expenses = query(collection(db, EXPENSES_COLLECTION), where("uid", "==", uid));
+    const expenses = collection(db, USERS_COLLECTION, uid, EXPENSES_COLLECTION);
     const querySnapshot = await getDocs(expenses);
     let allExpenses = [];
     for (const documentSnapshot of querySnapshot.docs) {
@@ -20,10 +25,36 @@ export async function getExpense(uid) {
             cost: expense.cost,
         })
     }
-    return allExpenses;  
+    return allExpenses;
+
+    /*
+        const expenses = query(collection(db, EXPENSES_COLLECTION), where("uid", "==", uid));
+        const querySnapshot = await getDocs(expenses);
+        let allExpenses = [];
+        for (const documentSnapshot of querySnapshot.docs) {
+            const expense = documentSnapshot.data();
+            allExpenses.push({
+                id: expense.id,
+                name: expense.name,
+                cost: expense.cost,
+            })
+        }
+        return allExpenses;
+    */
 }
 
-export async function deleteExpense(id) {
+export async function deleteExpense(uid, id) {
+    /*
+    const toDelete = query(collection(db, USERS_COLLECTION, uid, EXPENSES_COLLECTION), where("id", "==", id));
+    const querySnapshot = await getDocs(toDelete);
+    let docId;
+    for (const documentSnapshot of querySnapshot.docs) {
+        docId = documentSnapshot.id;
+    }
+    */
+    deleteDoc(doc(db, USERS_COLLECTION, uid, EXPENSES_COLLECTION, id));
+
+    /*
     const toDelete = query(collection(db, EXPENSES_COLLECTION), where("id", "==", id));
     const querySnapshot = await getDocs(toDelete);
     let docId;
@@ -31,23 +62,51 @@ export async function deleteExpense(id) {
         docId = documentSnapshot.id;
     }
     deleteDoc(doc(db, EXPENSES_COLLECTION, docId));
+    */
 }
 
 export async function editBudget(uid, budget) {
+    await setDoc(doc(db, USERS_COLLECTION, uid, BUDGET_COLLECTION, uid), { uid, budget });
+
+
+
+    /*
     const expenses = query(collection(db, BUDGET_COLLECTION), where("uid", "==", uid));
     const querySnapshot = await getDocs(expenses);
     let docId;
     for (const documentSnapshot of querySnapshot.docs) {
         docId = documentSnapshot.id;
     }
-    if (!docId) {
-        addDoc(collection(db, BUDGET_COLLECTION), { uid, budget });
-    } else {
-        updateDoc(doc(db, BUDGET_COLLECTION, docId), { budget: budget})
-    }
-}  
+    await setDoc(doc(db, BUDGET_COLLECTION, uid), { uid, budget });
+*/
+}
+
+/*
+const expenses = query(collection(db, BUDGET_COLLECTION), where("uid", "==", uid));
+const querySnapshot = await getDocs(expenses);
+let docId;
+for (const documentSnapshot of querySnapshot.docs) {
+    docId = documentSnapshot.id;
+}
+if (!docId) {
+    addDoc(collection(db, BUDGET_COLLECTION), { uid, budget });
+} else {
+    updateDoc(doc(db, BUDGET_COLLECTION, docId), { budget: budget })
+}
+*/
+
 
 export async function getBudget(uid) {
+    const expenses = collection(db, USERS_COLLECTION, uid, BUDGET_COLLECTION);
+    const querySnapshot = await getDocs(expenses);
+    let budget;
+    for (const documentSnapshot of querySnapshot.docs) {
+        budget = documentSnapshot.data().budget;
+    }
+    return budget;
+
+
+    /*
     const expenses = query(collection(db, BUDGET_COLLECTION), where("uid", "==", uid));
     const querySnapshot = await getDocs(expenses);
     let budget;
@@ -55,4 +114,5 @@ export async function getBudget(uid) {
         budget = documentSnapshot.data().budget;
     }
     return budget;
+    */
 }  
